43f4fcd78032d50d0569665ef019a69f32ca7c9e
Gencarmedia <149217218+GencarMedia@users.noreply.github.com>
2023-12-27 14:09:08 +0700

    Update index.js

--- index.js
+++ index.js
@@ -17,3 +17,86 @@ window.onscroll = () => {
   menu.classList.remove('bx-x'); 
   navlist.classList.remove('active');
 };
+// a
+const ctx = document.getElementById("revenues");
+
+Chart.defaults.color = "#fff",
+Chart.defaults.font.family = "Open Sans";
+
+new Chart(ctx, {
+  type: "bar",
+  data: {
+    labels: [
+      "Web Developer",
+      "Phographer",
+      "Desaigner",
+      "Editing",
+      "Problem Solving",
+      "Communication",
+      "Teamwork",
+      "Crative",
+    ],
+    datasets: [
+      {
+        label: "Revenue",
+        data: [
+          65, 90, 42, 70, 78, 40, 82, 65,
+        ],
+        backgroundColor: "aqua",
+        borderRadius: 6,
+        borderSkipped: false,
+      },
+    ],
+  },
+  // continuation
+
+  options: {
+    responsive: true,
+    maintainAspectRatio: false,
+    plugins: {
+      legend: {
+        display: false,
+      },
+      title: {
+        display: true,
+        text: "skills & Professional",
+        padding: {
+          bottom: 16,
+        },
+        font: {
+          size: 16,
+          weight: "normal",
+        },
+      },
+      tooltip: {
+        backgroundColor: "#111",
+      },
+    },
+    scales: {
+      x: {
+        border: {
+          dash: [2, 4],
+        },
+        grid: {
+          color: "#111",
+        },
+        title: {
+          text: "2023",
+        },
+      },
+      y: {
+        grid: {
+          color: "#111",
+        },
+        border: {
+          dash: [2, 4],
+        },
+        beginAtZero: true,
+        title: {
+          display: true,
+          text: "Revenue",
+        },
+      },
+    },
+  },
+});
